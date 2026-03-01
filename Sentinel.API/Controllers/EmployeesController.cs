af3b62ef5ae5f03c8a5dae680dacb749c4da0181
Glyn Davies <glynwdavies@gmail.com>
2026-03-01 20:26:03 +0000

    Added empty EmployeesController.

---
+++ Sentinel.API/Controllers/EmployeesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sentinel.API.Controllers;
+
+[ApiController]
+[Route("api/employees")]
+public class EmployeesController : ControllerBase
+{
+    // TODO: Implement EmployeesController
+}
