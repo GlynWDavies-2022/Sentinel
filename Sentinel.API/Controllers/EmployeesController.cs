80cb30979a5805c7f9ebcd04d070b8630212c2c1
Glyn Davies <glynwdavies@gmail.com>
2026-03-11 13:20:14 +0000

    Added Employee endpoints.

--- Sentinel.API/Controllers/EmployeesController.cs
+++ Sentinel.API/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sentinel.API.Data;
 
 namespace Sentinel.API.Controllers;
 
@@ -6,5 +7,22 @@ namespace Sentinel.API.Controllers;
 [Route("api/employees")]
 public class EmployeesController : ControllerBase
 {
-    // TODO: Implement EmployeesController
+    [HttpGet]
+    public ActionResult GetEmployees()
+    {
+        return Ok(EmployeesDataStore.Current.Employees);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult GetEmployeeById(int id)
+    {
+        var employee = EmployeesDataStore.Current.Employees.FirstOrDefault(e => e.Id == id);
+
+        if (employee == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(employee);
+    } 
 }
--- Sentinel.API/Data/EmployeesDataStore.cs
+++ Sentinel.API/Data/EmployeesDataStore.cs
@@ -12,7 +12,30 @@ public class EmployeesDataStore
     {
         Employees =
         [
-            new EmployeeDTO()
+            new EmployeeDTO 
+            {
+                Id = 1,
+                FirstName = "Erin",
+                LastName = "Reagan",
+                Company = "Domicile",
+                StaffNumber = "111111"
+            },
+            new EmployeeDTO
+            {
+                Id = 2,
+                FirstName = "Garrett",
+                LastName = "Moore",
+                Company = "Beachfront",
+                StaffNumber = "111112"
+            },
+            new EmployeeDTO
+            {
+                Id = 3,
+                FirstName = "Abigail",
+                LastName = "Baker",
+                Company = "Lakeside",
+                StaffNumber = "111113"
+            },
         ];
     }
 }   
